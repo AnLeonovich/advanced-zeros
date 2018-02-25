25df42f7b68c12b1b0c0d8a18fa9165f86a27204
AnLeonovich <novka666@mail.ru>
2018-02-25 19:36:02 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    
+    var obj = {};
+    var arr = [];
+    var zeros; 
+    var i = 2;
+
+    while (i < base){
+
+        if (base % i == 0) {
+        
+            if(obj.hasOwnProperty(i) == true){
+                obj[i] += 1;          
+            } else {
+                obj[i] = 1;
+            }
+            
+        base = base / i;
+        } else {
+            i++;
+        }
+    }
+
+    if(obj.hasOwnProperty(i) == true){
+        obj[i] += 1;         
+    } else {
+        obj[i] = 1;
+    }
+
+    for (var key in obj){
+        i=1;
+        zeros = 0; 
+        while(Math.pow(key, i)<number){
+            zeros += Math.floor(number/Math.pow(key, i));
+            i++;       
+        }
+    arr.push (Math.floor(zeros/obj[key]));
+    }
+
+    return Math.min.apply(null, arr);
 }
